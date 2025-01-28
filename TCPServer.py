7eee310a4ca10e86a29e52b55b1cdf68f5c1b67a
R-uslan <196853087+R-uslan@users.noreply.github.com>
2025-01-29 00:26:45 +0200

    Add basic TCP server

---
+++ TCPServer.py
@@ -0,0 +1,44 @@
+import socket
+
+
+class TCPServer:
+    def __init__(self, host: str = "127.0.0.1", port: int = 8080):
+        self.address = (host, port)
+        self.running = False
+        self.socket: socket.socket = None
+        self.buffer_size = 4096  # Used for recv
+
+    def start(self):
+        """
+        Creates a socket, listens on the specified address, and accepts connections.
+        """
+        self.running = True
+        print(f"Starting server on {self.address}...")
+
+        self.s = socket.create_server(self.address, reuse_port=True)
+
+        while self.running:
+            conn, addr = self.s.accept()
+            print(f"Connection from {addr}:{conn}")
+            self.handle_connection(conn, addr)
+            conn.close()
+
+    def stop(self):
+        """
+        Gracefully shutdown server.
+        """
+        self.running = False
+        if self.socket:
+            self.socket.close()
+
+    def handle_connection(self, conn, addr):
+        """
+        Handle the TCP connection. To be overridden by inheritor.
+        """
+        conn.recv(self.buffer_size)
+        conn.sendall(b"Connection received")
+
+
+if __name__ == "__main__":
+    server = TCPServer()
+    server.start()
